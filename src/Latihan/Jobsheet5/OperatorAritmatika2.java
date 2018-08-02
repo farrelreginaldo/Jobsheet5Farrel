dcdf90ba0fd3e0b33e1a9c90f029e5a1f8a474d9
Farrel <Farrel@LAPTOP-RLK1BSK1>
2018-08-02 13:38:57 +0700

    Initial Commit

---
+++ src/Latihan/Jobsheet5/OperatorAritmatika2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan.Jobsheet5;
+
+/**
+ *
+ * @author Farrel
+ */
+public class OperatorAritmatika2 {
+    public static void main(String[] args) {
+        double x = 10.0;
+        double y = 3.0;
+        double z = 2.5;
+        double a = 0.0;
+        double b = 0.0;
+        // Tambahkan sintaks operasi matemmatika di baris ini
+        a = x / y;
+        System.out.println( x + " : " + y + " = " + a );
+        b = x / z;
+        System.out.println( x + " : " + z + " = " + b );
+        
+    }
+}
---
+++ src/Latihan/Jobsheet5/OperatorArtimatika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan.Jobsheet5;
+
+/**
+ *
+ * @author Farrel
+ */
+public class OperatorArtimatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c;
+        // Penjumlahan
+        c = a + b;
+        System.out.println( a + " + " + b + " = " + c );
+        // Pengurangan
+        c = a - b;
+        System.out.println( a + " - " + b + " = " + c );
+        // Perkalian
+        c = a * b;
+        System.out.println( a + " x " + b + " = " + c );
+        // Sisa pembagian
+        c = a / b;
+        System.out.println( a + " : " + b + " = " + c );
+    }
+}
